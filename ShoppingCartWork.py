cc112032cf64a2e18ab23263a0ed1196b5516dee
aulorbe <aulorbe@gmail.com>
2018-10-30 06:55:10 -0400

    total is still wonky...giving me -20 each time, but almost finished

--- ShoppingCartWork.py
+++ ShoppingCartWork.py
@@ -2,13 +2,26 @@ from shopping_cart import ShoppingCart
 
 shopping_cart = ShoppingCart()
 
-print(shopping_cart.total)
-print(shopping_cart.employee_discount)
+shopping_cart.add_item("rainbow sandals", 45.99)
+shopping_cart.add_item("agyle socks", 10.50)
+shopping_cart.add_item("jeans", 50.00, 3)
+shopping_cart.add_item("plants", 20.00, 2)
 
-print(shopping_cart.add_item("rainbow sandals", 45.99)) # 45.99
-print(shopping_cart.add_item("agyle socks", 10.50)) # 56.49
-print(shopping_cart.add_item("jeans", 50.00, 3)) # 206.49
-print(shopping_cart.add_item("plants", 20.00, 2)) #246.49
+# print(shopping_cart.add_item("rainbow sandals", 45.99)) # 45.99
+# print(shopping_cart.add_item("agyle socks", 10.50)) # 56.49
+# print(shopping_cart.add_item("jeans", 50.00, 3)) # 206.49
+# print(shopping_cart.add_item("plants", 20.00, 2)) #246.49
+# print(shopping_cart.add_item("plants", 10.00))
+
+shopping_cart.get_total()
+
 
 shopping_cart.median_item_price() # 50.00  <<< how is that the answer to the median question? should be $45.99 when have the 1st 3 items in cart
-shopping_cart.mean_item_price() 
+shopping_cart.mean_item_price()
+
+discount_shopping_cart = ShoppingCart(20)
+print(shopping_cart.apply_discount())
+
+print(shopping_cart.item_names())
+
+print(shopping_cart.void_last_item())
--- shopping_cart.py
+++ shopping_cart.py
@@ -92,3 +92,34 @@ class ShoppingCart:
                 mean = statistics.mean(sorting)
                 rounded = round(mean,2)
                 print(rounded)
+
+# applies a discount if one is provided and returns the discounted total. For example, if we initialize a new shopping cart with a discount
+# of 20% then our total should be discounted in the amount of 20%. So, if our total were $100, after the discount we only would owe $80.
+# If our shopping cart does not have an employee discount, then it should return a string saying: "Sorry, there is no discount to apply to your cart :("
+
+    def apply_discount(self):
+        if self._employee_discount:
+            discount = self.employee_discount/100
+            disc_total = self.total * (1 - discount)
+            return disc_total
+        else:
+            return "Sorry, there is no discount to apply to your cart :("
+
+
+# Let's define an instance method called item_names which returns a list of names which represent each item we have in our cart --
+# if there are three socks the list should contain three "socks".
+
+    def item_names(self):
+        names = []
+        for i in self._items:
+            names.append(i['item_name'])
+        return names
+
+# Let's define a method called void_last_item that removes the last item from our shopping cart and updates its total.
+# If there are no items in the shopping cart, void_last_item should return "There are no items in your cart!".
+    def void_last_item(self):
+        if self._items:
+            removed_item = self._items.pop()
+        else:
+            return "There are no items in your cart!"
+        self.total -= removed_item['price']
